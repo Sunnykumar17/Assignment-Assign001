266c48aba06083813367e264b3152748c5a65424
Sunny kumar <98548654+Sunnykumar17@users.noreply.github.com>
2022-10-29 09:11:15 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import ReactDom from 'react-dom';
+import './index.css';
+
+ReactDom.render(
+  <div className='app'>
+    <h1>About us</h1>  
+    <div className='para'>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aperiam unde mollitia nulla, deserunt esse, nisi odio magni sint cum vitae voluptates! Possimus atque ducimus velit, dolore sapiente modi ut.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo itaque quo eius reiciendis facere error explicabo, iste eligendi quam quibusdam perferendis ipsam blanditiis, esse adipisci? Non magni nemo iste illum, aliquid eos unde in, labore veniam neque voluptates eveniet ipsa obcaecati deleniti odio exercitationem temporibus ea ab vel. Cupiditate praesentium aliquam in magni expedita doloremque sint, ducimus at accusamus esse impedit. At, maxime deserunt alias facilis soluta aliquid placeat explicabo velit fuga dolores ut, libero obcaecati nobis autem vel. Numquam, libero. Tempore illum dolorem dolores ullam? Sequi facere adipisci animi explicabo quaerat delectus fugiat, culpa nulla omnis quisquam quos reprehenderit non sit repellendus, eveniet libero dolor quidem? Reiciendis harum dolore, neque impedit quam suscipit aperiam totam nisi quisquam quae veniam magni eveniet obcaecati. Possimus, fuga quidem nam quia deleniti nobis eaque soluta illo id dolore similique odio mollitia modi accusantium sunt ea explicabo exercitationem iusto maiores rem praesentium quisquam atque?
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias accusantium dignissimos error ducimus iusto tenetur atque repudiandae a quasi voluptatum?</p>
+    </div>
+  
+  </div>,
+  document.getElementById("root")
+
+);
